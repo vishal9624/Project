2b45d713527f6b67e58d43f33241166a1f9c66a5
vishal9624 <vishal.dharmare24@gmail.com>
2022-07-04 19:32:47 +0530

    c

--- provider.tf
+++ provider.tf
@@ -5,8 +5,14 @@ terraform {
       version = "4.21.0"
     }
   }
+ backend "s3" {
+    bucket = "assignmentterraform"
+    key    = "assignment/tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
   # Configuration options
+  region = "us-east-1"
 }
