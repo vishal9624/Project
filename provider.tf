9219165e0ff596dcef180f57e69bd5d3de9c8178
vishal9624 <vishal.dharmare24@gmail.com>
2022-07-04 17:13:44 +0530

    ch

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.16.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
