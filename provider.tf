260f3f70af771f4443c8710ae5679b0fcd63891a
vishal9624 <vishal.dharmare24@gmail.com>
2022-07-04 19:27:07 +0530

    c

--- provider.tf
+++ provider.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.16.0"
+      version = "4.21.0"
     }
   }
 }
 
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
 }
